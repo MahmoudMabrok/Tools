153a9e2fc52d7dcf0d30e8390321a7584c1252df
MahmoudMabrok <mahmoudmabrok3579@gmail.com>
2019-10-13 21:54:19 +0200

    Add example for toasts

--- app/src/main/java/com/mahmoudmabrok/tools/TestToast.java
+++ app/src/main/java/com/mahmoudmabrok/tools/TestToast.java
@@ -2,9 +2,8 @@ package com.mahmoudmabrok.tools;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.LinearLayout;
+import android.widget.Button;
 
 import com.mahmoudmabrok.toasty.Toasty;
 
@@ -18,22 +17,38 @@ public class TestToast extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_toast);
 
-        Toasty.showMessage(this, "HI");
-
-        LinearLayout linearLayout = findViewById(R.id.root);
-        linearLayout.setOnTouchListener(new View.OnTouchListener() {
+        Button btnCenter = findViewById(R.id.btnAtCenter);
+        btnCenter.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if (event.getAction() == MotionEvent.ACTION_UP) {
-                    float x = event.getX();
-                    float y = event.getY();
-                    Toasty.showMessageAtXY(TestToast.this, "A", (int) x, (int) y);
-                }
-                return true;
+            public void onClick(View v) {
+                Toasty.showMessageAtCenter(TestToast.this, "Center");
             }
         });
 
-        Toasty.success(this, "HA").show();
+        Button btnAtXY = findViewById(R.id.btnAtXY);
+        btnAtXY.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toasty.showMessageAtXY(TestToast.this, "XY", 120, 254);
+            }
+        });
+
+        Button btnSuccess = findViewById(R.id.btnSuccess);
+        btnSuccess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toasty.success(TestToast.this, "Success").show();
+            }
+        });
+
+        Button btnError = findViewById(R.id.btnError);
+        btnError.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toasty.error(TestToast.this, "Error").show();
+            }
+        });
+
 
     }
 }
